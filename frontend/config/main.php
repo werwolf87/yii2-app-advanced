7acb7a8776b9912425dc3943630fc879f247b0b6
werwolf87 <michael.n.zak@gmail.com>
2015-02-04 17:30:35 +0300

    Pretty URL

--- frontend/config/main.php
+++ frontend/config/main.php
@@ -1,32 +1,42 @@
 <?php
 $params = array_merge(
-    require(__DIR__ . '/../../common/config/params.php'),
-    require(__DIR__ . '/../../common/config/params-local.php'),
-    require(__DIR__ . '/params.php'),
-    require(__DIR__ . '/params-local.php')
+	require(__DIR__ . '/../../common/config/params.php'),
+	require(__DIR__ . '/../../common/config/params-local.php'),
+	require(__DIR__ . '/params.php'),
+	require(__DIR__ . '/params-local.php')
 );
 
 return [
-    'id' => 'app-frontend',
-    'basePath' => dirname(__DIR__),
-    'bootstrap' => ['log'],
-    'controllerNamespace' => 'frontend\controllers',
-    'components' => [
-        'user' => [
-            'identityClass' => 'common\models\User',
-            'enableAutoLogin' => true,
-        ],
-        'log' => [
-            'traceLevel' => YII_DEBUG ? 3 : 0,
-            'targets' => [
-                [
-                    'class' => 'yii\log\FileTarget',
-                    'levels' => ['error', 'warning'],
-                ],
+	'id' => 'app-frontend',
+	'basePath' => dirname(__DIR__),
+	'bootstrap' => ['log'],
+	'controllerNamespace' => 'frontend\controllers',
+	'components' => [
+		'user' => [
+			'identityClass' => 'common\models\User',
+			'enableAutoLogin' => true,
+		],
+		'log' => [
+			'traceLevel' => YII_DEBUG ? 3 : 0,
+			'targets' => [
+				[
+					'class' => 'yii\log\FileTarget',
+					'levels' => ['error', 'warning'],
+				],
+			],
+		],
+		'errorHandler' => [
+			'errorAction' => 'site/error',
+		],
+		'urlManager' => [
+            'class' => 'yii\web\UrlManager',
+            'enablePrettyUrl' => true,
+            'showScriptName' => false,
+            'rules' => [
+                ' < _c:[\w\-] +>/<id:\d +>' => ' < _c>/view',
+                ' < _c:[\w\-] +>' => ' < _c>/index',
+                ' < _c:[\w\-] +>/<_a:[\w\-] +>/<id:\d +>' => ' < _c>/<_a > ',
             ],
-        ],
-        'errorHandler' => [
-            'errorAction' => 'site/error',
         ],
     ],
     'params' => $params,
